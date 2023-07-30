5ff3ce8390af0e2166a2fd314c1a95ce79207401
95Kiryl <Posakh95@mail.ru>
2023-07-30 14:13:37 +0300

    vowels

--- script.js
+++ script.js
@@ -112,3 +112,25 @@ function treeSum(arr) {
 }
 
 console.log(treeSum(arr));
+
+// Домашняя N.07 Домашнее задание VOWELS
+
+const newString = prompt('Введите строку и узнайте количество гласных букв в ней', '');
+
+function getVowels(str) {
+    str = str.toLowerCase();
+    let meaning = '';
+    let quantity = 0;
+    const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            quantity += 1;
+            meaning += str[i];
+        } else {
+            continue;
+        }
+    }
+    console.log(`В строке ${quantity} гласных, ${meaning}`);
+}
+
+console.log(getVowels(newString));
