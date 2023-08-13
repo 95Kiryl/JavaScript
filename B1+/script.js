42fd12a7fb5cf1eb38fbcc0dc9167ac501f6dc15
95Kiryl <Posakh95@mail.ru>
2023-08-13 23:42:28 +0300

    B1+

---
+++ B1+/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let year;
+
+do {
+    year = Number(prompt('Введите год', ''));
+} while (isNaN(year) || Number.isInteger(year) === false || year <= 0);
+
+function getСentury(year) {
+    const century = Math.ceil(year / 100);
+    return century;
+}
+
+getСentury(year);
+
+alert(`${century} век`);
+
