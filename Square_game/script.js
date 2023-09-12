1cb7459518a8012ca26ee055ba439733ea6beb6f
95Kiryl <Posakh95@mail.ru>
2023-09-12 09:43:34 +0300

    square

--- Square_game/script.js
+++ Square_game/script.js
@@ -9,38 +9,30 @@ window.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
         case 37:
             if (x <= 0) {
-                x -= 0;
-                square.style.left = x + 'px';
+                square.style.left = (x -= 0) + 'px';
             } else {
-                x -= 5;
-                square.style.left = x + 'px';
+                square.style.left = (x -= 5) + 'px';
             }
             break;
         case 38:
             if (y <= 0) {
-                y -= 0;
-                square.style.top = y + 'px';
+                square.style.top = (y -= 0) + 'px';
             } else {
-                y -= 5;
-                square.style.top = y + 'px';
+                square.style.top = (y -= 5) + 'px';
             }
             break;
         case 39:
             if (x >= 326) {
-                x += 0;
-                square.style.left = x + 'px';
+                square.style.left = (x += 0) + 'px';
             } else {
-                x += 5;
-                square.style.left = x + 'px';
+                square.style.left = (x += 5) + 'px';
             }
             break;
         case 40:
             if (y >= 326) {
-                y += 0;
-                square.style.top = y + 'px';
+                square.style.top = (y += 0) + 'px';
             } else {
-                y += 5;
-                square.style.top = y + 'px';
+                square.style.top = (y += 5) + 'px';
             }
             break;
     }
