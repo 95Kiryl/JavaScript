ff5f3012ad38d1f67b20cfaddb41090bc64e62bb
95Kiryl <Posakh95@mail.ru>
2023-09-16 13:58:07 +0300

    B4

--- B4+/script.js
+++ B4+/script.js
@@ -1,54 +1,36 @@
 'use strict'
 
-function buildWrapper(str) {
-    let newStr = '';
-    const obj = {
-        '<': '&lt;',
-        '>': '&gt;',
-        "'": '&apos;',
-        '"': '&quot;',
-        '&': '&amp;',
-    }
-    for (let i = 0; i < str.length; i++) {
-        if (obj.hasOwnProperty(str[i])) {
-            newStr += obj[str[i]];
-        } else {
-            newStr += str[i];
+function buildWrapper(tag) {
+    return function wrapper(str) {
+        let newStr = '';
+        const obj = {
+            '<': '&lt;',
+            '>': '&gt;',
+            "'": '&apos;',
+            '"': '&quot;',
+            '&': '&amp;',
         }
-    }
+        for (let i = 0; i < str.length; i++) {
+            if (obj.hasOwnProperty(str[i])) {
+                newStr += obj[str[i]];
+            } else {
+                newStr += str[i];
+            }
+        }
 
-    let resultH1 = document.createElement('h1');
-    resultH1.textContent = newStr;
-    return console.log(resultH1);
+        return `<${tag}>${newStr}</${tag}>`
+    }
 }
 
-const wrapH1 = buildWrapper('стихи');
+const wrapH1 = buildWrapper('H1');
+const wrapP = buildWrapper("P");
+
+console.log(wrapH1('СТИХИ'));
+console.log(wrapP('Однажды в студеную зимнюю пору'));
+console.log(wrapP("Вкусные M&M's"));
 
 
-function buildWrapperP(string) {
-    let newString = '';
-    const obj = {
-        '<': '&lt;',
-        '>': '&gt;',
-        "'": '&apos;',
-        '"': '&quot;',
-        '&': '&amp;',
-    }
 
-    for (let i = 0; i < string.length; i++) {
-        if (obj.hasOwnProperty(string[i])) {
-            newString += obj[string[i]];
-        } else {
-            newString += string[i];
-        }
-    }
-    let resultP = document.createElement('p');
-    resultP.textContent = newString;
-    return console.log(resultP);
-}
-
-// const wrapP = buildWrapperP('Однажды в студеную зимнюю пору');
-const wrapP = buildWrapperP("Вкусные M&M's");
 
 
 
