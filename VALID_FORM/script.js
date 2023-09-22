1e51ef17869b350f8192839ef3c824d9ce1b5f35
95Kiryl <Posakh95@mail.ru>
2023-09-23 00:14:39 +0300

    validation

--- VALID_FORM/script.js
+++ VALID_FORM/script.js
@@ -13,25 +13,32 @@ const form = document.forms.form,
     description = document.querySelector('#description'),
     submit = document.querySelector('[name="submit"]'),
     wrapp = document.querySelector('.wrapp');
-
+let caseSpan = null;
 
 
 const addError = (event) => {
-    const spanError = document.createElement('span');
-    spanError.className = event.target.name;
-    if (event.target.name === 'visitors') {
-        spanError.textContent = 'Только числовые данные';
-    } else if (event.target.name === 'data') {
-        spanError.textContent = 'Укажите дату';
+    getCaseError(event.target.name);
+    if (caseSpan !== null) {
+        event.preventDefault();
+        return;
     } else {
-        spanError.textContent = 'Длина строки должна быть от 1 до 50 символов';
+        const spanError = document.createElement('span');
+        spanError.className = event.target.name;
+        if (event.target.name === 'visitors') {
+            spanError.textContent = 'Только числовые данные';
+        } else if (event.target.name === 'data') {
+            spanError.textContent = 'Укажите дату';
+        } else {
+            spanError.textContent = 'Длина строки должна быть от 1 до 50 символов';
+        }
+        spanError.style.color = 'red';
+        event.target.insertAdjacentElement('afterend', spanError);
+        event.target.style.borderColor = 'red';
+        event.preventDefault();
+        return;
     }
-    spanError.style.color = 'red';
-    event.target.insertAdjacentElement('afterend', spanError);
-    event.target.style.borderColor = 'red';
-    event.preventDefault();
-    return;
 }
+
 
 const searchError = (event) => {
     const inputError = document.querySelectorAll('span');
@@ -49,30 +56,82 @@ const removeError = (event, eventError) => {
 }
 
 const addErrorSubmit = (event, field) => {
-    const submitError = document.createElement('span');
-    if (field.className === 'accommodationField') {
-        submitError.className = 'accommodation';
+    if (!field.name) {
+        caseSpan = document.querySelector('.accommodation');
     } else {
-        submitError.className = field.name;
+        getCaseError(field.name);
     }
-    submitError.textContent = ` ЗАПОЛНИТЕ ПОЛЕ`;
-    submitError.style.color = 'red';
-    field.insertAdjacentElement('afterend', submitError);
-    field.style.borderColor = 'red';
-    if (field.id === 'description') {
+    if (caseSpan !== null) {
+        scrollToInput(event, field);
+    } else {
+        const submitError = document.createElement('span');
+        if (field.className === 'accommodationField') {
+            submitError.className = 'accommodation';
+        } else {
+            submitError.className = field.name;
+        }
+        if (submitError.className === 'visitors') {
+            submitError.textContent = 'ЗАПОЛНИТЕ ПОЛЕ - Только числовые данные';
+        } else if (submitError.className === 'data') {
+            submitError.textContent = 'ЗАПОЛНИТЕ ПОЛЕ - Укажите дату';
+        } else {
+            submitError.textContent = 'ЗАПОЛНИТЕ ПОЛЕ - от 1 до 50 символов';
+        }
+        submitError.style.color = 'red';
+        field.insertAdjacentElement('afterend', submitError);
+        field.style.borderColor = 'red';
+        scrollToInput(event, field);
+    }
+}
+
+const scrollToInput = (event, field) => {
+    const firstError = document.querySelectorAll('span');
+    const spanName = firstError[0].className;
+    const inputs = document.querySelectorAll('input');
+    const arrayInputs = Array.from(inputs);
+    const firstErrorInput = arrayInputs.filter(i => i.name === spanName);
+    if (field.id === 'description' && !firstErrorInput[0]) {
         description.focus();
         event.preventDefault();
         return;
     } else {
-        const firstError = document.querySelectorAll('span');
-        const spanName = firstError[0].className;
-        const inputs = document.querySelectorAll('input');
-        const arrayInputs = Array.from(inputs);
-        const firstErrorInput = arrayInputs.filter(i => i.name === spanName);
         firstErrorInput[0].focus();
         event.preventDefault();
         return;
     }
+}
+
+const getCaseError = (nameError) => {
+    switch (nameError) {
+        case 'developers':
+            caseSpan = document.querySelector('.developers');
+            break;
+        case 'name':
+            caseSpan = document.querySelector('.name');
+            break;
+        case 'url':
+            caseSpan = document.querySelector('.url');
+            break;
+        case 'data':
+            caseSpan = document.querySelector('.data');
+            break;
+        case 'visitors':
+            caseSpan = document.querySelector('.visitors');
+            break;
+        case 'mail':
+            caseSpan = document.querySelector('.mail');
+            break;
+        case 'accommodation':
+            caseSpan = document.querySelector('.accommodation');
+            break;
+        case 'description':
+            caseSpan = document.querySelector('.description');
+            break;
+        case 'submit':
+            caseSpan = document.querySelector('.submit');
+            break;
+    }
+    return caseSpan;
 }
 
 developers.addEventListener('blur', (event) => {
@@ -143,18 +202,23 @@ form.addEventListener('submit', (event) => {
     if (developers.value.length <= 0 || developers.value.length > 50) {
         addErrorSubmit(event, developers);
     }
+
     if (name.value.length <= 0 || name.value.length > 50) {
         addErrorSubmit(event, name);
     }
+
     if (url.value.length <= 0 || url.value.length > 50) {
         addErrorSubmit(event, url);
     }
+
     if (data.value === '') {
         addErrorSubmit(event, data);
     }
+
     if (isNaN(visitors.value) || visitors.value === '') {
         addErrorSubmit(event, visitors);
     }
+
     if (mail.value.length <= 0 || mail.value.length > 50) {
         addErrorSubmit(event, mail);
     }
@@ -168,19 +232,13 @@ form.addEventListener('submit', (event) => {
             accommodationTrue += 1;
         }
     })
+
     if (accommodationTrue <= 0) {
         const accommodationField = document.querySelector('.accommodationField');
         addErrorSubmit(event, accommodationField);
     }
 
-
-
-
     if (description.value.length <= 0 || description.value.length > 50) {
         addErrorSubmit(event, description);
     }
 })
-
-
-
-
